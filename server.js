e5c21e5bc95d1fb6f3f2f3f7ca16d11ccc95e89a
monspear <tlcks1211@naver.com>
2024-12-11 12:10:55 +0900

    update

--- server.js
+++ server.js
@@ -56,24 +56,47 @@ app.get('/Register', (req, res) => {
 });
 // npm i mysql 로 mysql에 연결할 수 있게
 
+var flag = 0;
 app.post('/Register', (req, res) => {
     
     connection.connect();
+    var check_sql='select count(*) as cnt from react_node_express_user where nickname = ? and u_email = ?';
+    var check_values = [req.body.nickname, req.body.u_eamil];
+
+    connection.query(check_sql,check_values, function (error, result, fields) {
+        console.log("에러:"+error);
+        console.log("결과값입니다:"+JSON.stringify(result));
+        console.log("결과행"+result[0].cnt);
+        flag = result[0].cnt;
+        console.log("1flag:"+flag)
+        if (error){
+            console.log("서버단에서 회원정보 중복확인 중 에러발생"+error);
+        } else if(result[0].cnt != 0){ // 같은 유저정보가 존재한다면
+            console.log("1결과값입니다:"+JSON.stringify(result));
+            res.status(409).send(JSON.stringify('중복된 회원정보입니다'));
+        } 
+    });
+    //connection.end();
+    /*
+    
+    https://velog.io/@hseoy/%EC%9E%90%EC%A3%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-10%EA%B0%80%EC%A7%80-HTTP-Status-Code
+
+    */
     var sql = 'INSERT INTO react_node_express_user VALUES(NULL,?,?,?,?,?)';
     var values = [req.body.nickname, req.body.u_eamil, req.body.u_id, req.body.u_password, req.body.u_checked_password];
     console.log("받은 값 = "+values);
-    connection.query(sql,values, function (error, result, fields) {
-        if (error){
-            console.log("서버단에서 에러발생"+error);
-            connection.end();
-        }
-        console.log("Number of records inserted: " + result.affectedRows);
-    });
-     
+    console.log("flag:"+flag)
+    if(flag==0){
+        connection.query(sql,values, function (error, result, fields) {
+            if (error){
+                console.log("서버단에서 에러발생"+error);
+                //connection.end();
+            }
+            console.log("Number of records inserted: " + result.affectedRows);
+        }); 
+        res.status(200);
+    }
     connection.end();
-    res.status(200);
-
-
     //console.log("req = "+req.body);
     //console.log("res = "+res.body);
 });
